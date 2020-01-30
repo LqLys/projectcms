7e931ebed176f646074e31ef950c2be8e5d98aea
Lukasz <lys.lukasz555@gmail.com>
2020-01-30 23:48:58 +0100

    added exception handler

--- src/main/java/com/example/cms/auth/AuthenticationController.java
+++ src/main/java/com/example/cms/auth/AuthenticationController.java
@@ -11,10 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -165,6 +162,13 @@ public class AuthenticationController {
             modelAndView.setViewName("profile");
         }
         return modelAndView;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("error-page");
+        return mav;
     }
 
 
